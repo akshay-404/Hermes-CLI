916817f84e61947691a55ad41a1093bd8f2de148
akshay-404 <akshayanilparavur@gmail.com>
2026-08-31 00:53:47 +0530

    tls cert generation fix

--- tls-cert-gen.sh
+++ tls-cert-gen.sh
@@ -1,3 +1,3 @@
-mkdir -p server/certs
-openssl genrsa -out server/certs/server.key 4096
+cd server/certs
+openssl genrsa -out server.key 4096
 openssl req -new -x509 -key server.key -out server.crt -days 365 -config cert.conf -extensions v3_req
